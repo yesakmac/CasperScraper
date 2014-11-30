4f2e1a38e0a5e6c108239ed7e37c3325116c670a
yesak <yesakmac@gmail.com>
2014-11-30 10:31:15 -0600

    Updated filename for code file.

--- scrape.js
+++ scrape.js
@@ -17,7 +17,7 @@ casper.start('http://casperjs.org/', function() {
     console.log('starting');
     fs.write(filename, '{"scrapes": \r\n' + '\t"output": { \r\n',  'a');
     rollit(arrdata);
-    
+    console.log(JSON.stringify(arrdata);
 });
 //lets roll through the array
 
@@ -39,6 +39,7 @@ function rollit(arrdata){
         //can't get casper to initate from here....
             casper.thenOpen(site,function(){
                    console.log(this.getTitle());
+                   
                    fs.write(filename, '\t \t ["QUERYSTRING": "' + site + '" ], ["Title": "' + this.getTitle() + '"], \r\n',  'a');
                 });
 
